d24c6bab7e6ac79a9783a4a569b4deb0f02e30d2
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-23 08:55:51 +0300

    feat(TASK-777): Add exercise with IAsyncEnumerable

---
+++ Module2/AsyncEnumerable/Program.cs
@@ -0,0 +1,15 @@
+﻿await foreach (var num in GetNumbersAsync())
+{
+    Console.WriteLine(num);
+}
+
+return;
+
+async IAsyncEnumerable<int> GetNumbersAsync()
+{
+    for (var i = 0; i < 10; i++)
+    {
+        await Task.Delay(1000);
+        yield return i;
+    }
+}
