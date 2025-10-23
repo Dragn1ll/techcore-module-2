d273a2b3a6d999a2cdcc2221586d97d7eedb364e
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-23 10:02:42 +0300

    feat(TASK-777): Add ValueTask exercise

---
+++ Module2/ValueTask/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ValueTask;
+
+public class Program
+{
+    private static readonly Dictionary<int, string> Cache = new();
+    
+    static async Task Main()
+    {
+        Console.WriteLine("Первый вызов:");
+        var result1 = await GetDataAsync(1);
+        Console.WriteLine($"Результат: {result1}");
+        
+        Console.WriteLine("Второй вызов:");
+        var result2 = await GetDataAsync(1);
+        Console.WriteLine($"Результат: {result2}");
+    }
+
+    private static async ValueTask<string> GetDataAsync(int id)
+    {
+        if (Cache.TryGetValue(id, out var data))
+        {
+            return data;
+        }
+        
+        await Task.Delay(1000);
+        
+        data = $"Data for ID {id}";
+        Cache[id] = data;
+        
+        return data;
+    }
+}
