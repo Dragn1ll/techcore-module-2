2572c2f4337bee8c61cee732131861eb6eea5116
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-23 08:22:31 +0300

    feat(TASK-777): Add exercise with CancellationToken

---
+++ Module2/Cancellation/Program.cs
@@ -0,0 +1,17 @@
+﻿var cts = new CancellationTokenSource();
+
+DoSomething(cts.Token);
+await Task.Delay(2000);
+
+cts.Cancel();
+
+return;
+
+async Task DoSomething(CancellationToken token)
+{
+    while (!token.IsCancellationRequested)
+    {
+        await Task.Delay(1000, token);
+        Console.WriteLine("1");
+    }
+}
