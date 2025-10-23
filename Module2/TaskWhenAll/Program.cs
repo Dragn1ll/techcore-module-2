6f475b7b0f14d46630c18d2b916463bc84b7030c
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-23 08:08:30 +0300

    feat(TASK-777): Add exercise with Task.WhenAll

---
+++ Module2/TaskWhenAll/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+var tasks = new[] { Task.Run(() => Task.Delay(1000)), Task.Run(() => Task.Delay(1500)), 
+    Task.Run(() => Task.Delay(2000)) };
+
+Console.WriteLine("Начато выполнение tasks...");
+var sw = Stopwatch.StartNew();
+await Task.WhenAll(tasks);
+sw.Stop();
+Console.WriteLine($"tasks выполнились за: {sw.ElapsedMilliseconds}ms");
+
